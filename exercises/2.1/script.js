06d892ce32362a802a09afd5a197b2d6940703bb
MarcioFujita <hanker78@gmail.com>
2020-11-16 20:08:17 -0300

    Mudança no arquivo script.js na nova branch

--- exercises/2.1/script.js
+++ exercises/2.1/script.js
@@ -1,4 +1,4 @@
-let student = "euMesmo";
-let studentSkills = 10;
-
-console.log("Sucesso!");
+let pessoaEstudante = "euMesmo";
+let habilidadesPessoaEstudante = 10;
+//Eu sou Tryber e...
+console.log("sucesso");
