ea5ada4064bade19788b4f99553c5a676a0b993c
MarcioFujita <hanker78@gmail.com>
2021-01-12 19:41:23 -0300

    esqueci de comitar antes

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/scriptpart1.js
@@ -0,0 +1,21 @@
+const testingScope = (escopo) => { 
+    if (escopo === true) { 
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if).';
+      ifScope2 = 'Ótimo, fui utilizada no escopo !';
+      console.log(`${ifScope} ${ifScope2}`);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(`${elseScope}`);
+    }
+    // console.log(ifScope + ' o que estou fazendo aqui ? :O'); 
+    // Se necessário esta linha pode ser removida.
+  }
+  testingScope(true);
+
+// let oddsAndEvens = [13, 3, 4, 10, 7, 2].sort((a, b) => a-b)
+let oddsAndEvens = [13, 3, 4, 10, 7, 2]
+oddsAndEvens.sort((a, b) => a - b);
+
+      // Seu código aqui.
+
+console.log(oddsAndEvens);
---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/scriptpart2.js
@@ -0,0 +1,26 @@
+const fatorial = (numero) => {
+    let result = 0;
+    let numFor = numero - 1
+    for (let index = numFor; index > 0; index -= 1) {
+        result = result + (numero * index);
+    }
+    console.log(`O fatorial de ${numero} é: ${result}`);
+}
+fatorial(4);
+
+const maiorPalavra = (frase) => {
+    let separado = frase.split(' ');
+    let resultadoParcial = '';
+    let resultado = '';
+    let size = 0;
+    for (let index = 0; index < separado.length; index +=1) {
+        resultadoParcial = separado[index];
+        let tamanho = resultadoParcial.length;
+        if (tamanho > size) {
+            size = tamanho;
+            resultado = resultadoParcial;
+        }
+    }
+    console.log(`A maior palavra da frase é: ${resultado}`);
+}
+maiorPalavra('Antônio foi no banheiro e não sabemos o que aconteceu')
