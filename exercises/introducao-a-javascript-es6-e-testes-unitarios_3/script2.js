71b589cea2c90586865769fef0b347d9d2db4069
MarcioFujita <hanker78@gmail.com>
2021-01-14 18:52:28 -0300

    Part3 feita

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/script2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/script2.js
@@ -21,10 +21,16 @@ assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
 assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
 
 // Exercicio 4
+const myList = [5, 6, 7, 8, 9, 10];
+myRemove(myList, 10);
+assert.deepStrictEqual(myList, [5, 6, 7, 8, 9, 10]);
+
+// Exercicio 5
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
 
 
-// 1- Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
-// 2- Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-// 3- Verifique se o array passado por parâmetro não sofreu alterações
-// 4- Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+// 1- A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+// 2- Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// 3- Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// 4- Verifique se o array passado por parâmetro não sofreu alterações
+// 5- Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
