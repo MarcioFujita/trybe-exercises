79b49f37a450a64628de77450c038bbe289e6702
MarcioFujita <hanker78@gmail.com>
2021-01-27 18:47:44 -0300

    Exercicio 6 resolvido.

--- exercises/javascript-assincrono-e-promises_1/exercise6.js
+++ exercises/javascript-assincrono-e-promises_1/exercise6.js
@@ -12,6 +12,14 @@ const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now 
 const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
 
 // definição da função sendMarsTemperature...
+const sendMarsTemperature = (onSuccess, onError) => {
+    const temperature = getMarsTemperature();
+    const msgSent = Math.random() <= 0.6;
+    setTimeout(() => {
+        if(msgSent) onSuccess(temperature)
+        else onError('Robot is busy');
+    }, messageDelay());
+}
 
 
 // imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
