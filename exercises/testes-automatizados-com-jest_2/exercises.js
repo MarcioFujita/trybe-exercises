957381b8efa71f99dd4ed7d7d4c7a5151c87c998
MarcioFujita <hanker78@gmail.com>
2021-02-04 18:16:18 -0300

    Exercicio 2 feito

--- exercises/testes-automatizados-com-jest_2/exercises.js
+++ exercises/testes-automatizados-com-jest_2/exercises.js
@@ -11,24 +11,24 @@ const uppercase = (str, callback) => {
 // 3- Reescreva o teste do exercício anterior, desta vez utilizando a sintaxe de async/await .
 // Dica: Utilize o try/catch para o caso de erro.
 
-// const users = {
-//     4: { name: 'Mark' },
-//     5: { name: 'Paul' }
-//   };
+const users = {
+    4: { name: 'Mark' },
+    5: { name: 'Paul' }
+  };
   
-//   const findUserById = (id) => {
-//     return new Promise((resolve, reject) => {
-//         if (users[id]) {
-//           return resolve(users[id]);
-//         };
+  const findUserById = (id) => {
+    return new Promise((resolve, reject) => {
+        if (users[id]) {
+          return resolve(users[id]);
+        };
   
-//         return reject({ error: 'User with ' + id + ' not found.' });
-//     });
-//   };
+        return reject({ error: 'User with ' + id + ' not found.' });
+    });
+  };
   
-//   const getUserName = (userID) => {
-//     return findUserById(userID).then(user => user.name);
-//   };
+  const getUserName = (userID) => {
+    return findUserById(userID).then(user => user.name);
+  };
 
 // 4- O código abaixo busca no GitHub de um usuário, de acordo com a URL, seus repositórios, e retorna uma lista como resultado. Dada a URL 'https://api.github.com/orgs/tryber/repos' , faça um teste que verifique que os repositórios 'sd-01-week4-5-project-todo-list' e 'sd-01-week4-5-project-meme-generator' se encontram nessa lista.
 
@@ -98,4 +98,4 @@ const uppercase = (str, callback) => {
 
 
 
-module.exports = uppercase;
+module.exports = { uppercase, findUserById };
--- exercises/testes-automatizados-com-jest_2/exercises.test.js
+++ exercises/testes-automatizados-com-jest_2/exercises.test.js
@@ -1,13 +1,26 @@
 
-const { expect } = require('@jest/globals');
-const { doesNotMatch } = require('assert');
-const uppercase = require('./exercises');
+const { it, expect } = require('@jest/globals');
+const { uppercase, findUserById } = require('./exercises');
 
-describe('Testing function uppercase', () => {
+describe('EX1: Testing function uppercase', () => {
     it('receives a string and should convert it to uppercase', (done) => {
         uppercase('test', (str) => {
             expect(str).toBe('TEST');
             done();
           });
+    });
+});
+
+describe('Ex2: Testing function findUserById', () => {
+    it('receives an id and should return an user', () => {
+        expect.assertions(1);
+        return findUserById(4).then(user => {
+            expect(user.name).toEqual('Mark');
+        });
+    });
+    it('receives an invalid id and should return an error', () => {
+        return findUserById(3).catch(error => {
+            expect(error).toEqual({ error: 'User with 3 not found.' });
+        });
     });
 });
